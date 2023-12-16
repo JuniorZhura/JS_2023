5b88c9526f980d216be222595610d64fdecbda9c
Kseniia Zhuravel-Salva <123269592+KseniiaZhuravelSalva@users.noreply.github.com>
2023-12-16 21:03:49 +0200

    Delete lesson_6 directory

--- lesson_6/js/script_1.js
+++
@@ -1,6 +0,0 @@
-for (let numberO = 0; numberO < 20; numberO++) {
-document.write (
-      `<span>O</span>`
-)
-};
-// Використовуючи цикли вивести на екран 20 символів «о».
--- lesson_6/js/script_6.js
+++
@@ -1,6 +0,0 @@
-for (let number = 20; number <= 50; number++) {
-      if (number % 3 === 0 && number % 5 !== 0)
-      document.write(`<div>${number}</div>`)
-}
-
-// Дано натуральні числа від 20 до 50. Надрукувати ті з них, які діляться на 3, але не діляться на 5.
